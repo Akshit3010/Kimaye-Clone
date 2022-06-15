ab5a2ca267e5f543fc3ccfe584db9894f98c8556
jeny008 <jenytandel008@gmail.com>
2022-06-15 15:06:19 +0530

    homepage done

--- Kimaye-FE/kimaye-clone/src/App.js
+++ Kimaye-FE/kimaye-clone/src/App.js
@@ -1,16 +1,33 @@
-import "./App.css";
-import OurStory from "./Learn/OurStory";
+import './App.css';
+import NavbarPage from './components/NavbarPage';
+import Footer from './components/Footer';
+// import { OurStory } from './pages/OurStory';
+// import { Whykimaye } from './pages/Whykimaye';
+import { Route, Routes } from 'react-router-dom';
+import ControlledCarousel from './components/ControlledCarousel';
+import MainHome from './pages/MainHome';
 
 function App() {
-  return <div className="App">
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavbarPage />
+        <ControlledCarousel/>
+        <MainHome/>
 
-     <OurStory/>
+
+
+        <Routes>
+          {/* <Route path='ourstory' element={<OurStory />} /> */}
+          {/* <Route path='whykimaye' element={<Whykimaye />} /> */}
+        </Routes>
 
 
 
-
-
-  </div>;
+          <Footer />  
+      </header>
+    </div>
+  );
 }
 
 export default App;
