f694c43fc1e273c30f3ef77ef28166fad99d1869
GUDDU434 <99042645+GUDDU434@users.noreply.github.com>
2022-06-18 19:11:36 +0530

    Checkout page done

--- Kimaye-FE/kimaye-clone/src/Checkout/CheckOut.jsx
+++ Kimaye-FE/kimaye-clone/src/Checkout/CheckOut.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 import styled from "styled-components";
 
@@ -19,6 +21,7 @@ const Intro = styled.div`
 const Line = styled.div`
   display: flex;
   gap: 7px;
+  justify-content: space-between;
 `;
 
 const Link = styled.a`
@@ -27,17 +30,20 @@ const Link = styled.a`
 `;
 
 const Long = styled.input`
-  width: 70%;
-  height: 25px;
-  border-radius: 7px;
+  width: 95%;
+  height: 50px;
+  border-radius: 5px;
   border: none;
   padding: 7px;
+  margin-bottom: 10px;
 `;
 
 const Short = styled.input`
 width:30%;
-height:25px
+height:50px;
 border-radius:7px;
+border: none;
+
 padding:7px`;
 
 const Shiping = styled.div`
@@ -50,74 +56,133 @@ const Total = styled.div`
 `;
 
 const Checkout = () => {
+  const [email, setEmail] = useState("");
+  const [formData, setFormdata] = useState({});
+
+  useEffect(() => {
+    setEmail(localStorage.getItem("emailId"));
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormdata({ ...formData, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <Check>
       <Intro>
         <img
-          style={{ width: 200 }}
+          style={{ width: 200, margin: "30px 0px" }}
           src="https://cdn.shopify.com/s/files/1/0449/5225/6667/files/website-logo.png?19431"
           alt=""
         ></img>
-        <h4>
+        <p>
           <Link href="">Contact information</Link> {" >"}
           <Link href="">Shipping</Link> {">"} <Link href="">Payment</Link>{" "}
-        </h4>
+        </p>
         <div>
           <Line>
             <div>
-              <p>Contact information</p>
+              <p style={{fontSize:"25px"}}>Contact information</p>
             </div>
             <div>
               <p>
-                Already have an account? <Link href="">Log in</Link>
+                Already have an account? <Link href="/">Log in</Link>
               </p>
             </div>
           </Line>
-          <Long name="email" type="email" placeholder="Email" required />
+          <Long
+            name="email"
+            type="email"
+            // value={email}
+            placeholder="Email"
+            required
+          />
           <br />
           <input type="checkbox"></input>
           <label>Get order notification and delivery updates</label>
         </div>
         <Shiping>
-          <form>
-            <h4>Shipping address</h4>
+          <form onSubmit={handleSubmit}>
+            <h4 style={{marginTop:"40px",marginBottom:"30px"}}>Shipping address</h4>
             <Long
               name="country"
               type="text"
               placeholder="COUNTRY/REGION"
               required
+              onChange={handleChange}
             />
-            <br /> <br />
+            <br />
+            <div style={{display:"flex",justifyContent:"space-between",width:"95%",marginBottom:"10px"}}>
+
             <Short
+              style={{width:"49%"}}
               name="firstName"
               type="text"
               placeholder="First Name"
               required
+              onChange={handleChange}
             />
             <Short
+            style={{width:"49%"}}
               name="lastName"
               type="text"
               placeholder="Last Name"
               required
+              onChange={handleChange}
             />
-            <br /> <br />
-            <Long name="address" type="text" placeholder="Address" required />
-            <br /> <br />
+          </div>
+            <Long
+              name="address"
+              type="text"
+              placeholder="Address"
+              required
+              onChange={handleChange}
+            />
+            <br />
             <Long
               name="apartment"
               type="text"
               placeholder="Apartment,Suite,etc(optional)"
               required
+              onChange={handleChange}
             />
-            <br /> <br />
-            <Short name="city" type="text" placeholder="City" required />
-            <Short name="State" type="text" placeholder="State" required />
-            <Short name="zip" type="text" placeholder="Pin Code" />
-            <br /> <br />
+            <br />
+            <div style={{display:"flex",justifyContent:"space-between",width:"95%",marginBottom:"10px"}}>
+            <Short
+            style={{width:"33%"}}
+              name="city"
+              type="text"
+              placeholder="City"
+              required
+              onChange={handleChange}
+            />
+            <Short
+            style={{width:"31%"}}
+              name="State"
+              type="text"
+              placeholder="State"
+              required
+              onChange={handleChange}
+            />
+            <Short
+            style={{width:"33%"}}
+              name="zip"
+              type="text"
+              placeholder="Pin Code"
+              required
+              onChange={handleChange}
+            />
+            </div>
             <Long
               name="Number"
               type="number"
-              placeholder="Mobile number for order and delivery update(optional)"
+              placeholder="Mobile number for order and delivery update"
+              required
+              onChange={handleChange}
             />
             <br />
             <input type="checkbox"></input>
@@ -128,51 +193,66 @@ const Checkout = () => {
               style={{
                 color: "white",
                 backgroundColor: "#648c11",
-                padding: 14,
-                width: 170,
+                padding: 30,
+                width: 220,
                 borderRadius: 10,
                 border: "none",
               }}
               type="submit"
-              placeholder="Continue to Shipping"
             >
               Continue to Shipping
             </button>
+            <Link href="/cart" style={{ color: "red", marginLeft: "30px" }}>
+              Return to cart
+            </Link>
           </form>
         </Shiping>
+        <div style={{borderTop:"solid 2px gray", padding:"10px",display:"flex",marginTop:"50px",fontSize:"12px"}}>
+          <Link style={{marginRight:"20px"}} href="">Return policy</Link>
+          <Link style={{marginRight:"20px"}} href="">Shipping policy</Link>
+          <Link style={{marginRight:"20px"}} href="">Privacy policy</Link>
+          <Link style={{marginRight:"20px"}} href="">Terms of service</Link>
+        </div>
       </Intro>
       <Total>
-        <form>
+        <div>
+          Map the cart data here
+        </div>
           <hr />
-          <div style={{ display: "flex", padding: 10 }}>
+          <div style={{ display: "flex",justifyContent:"space-between"}}>
             <Long
               name="discount"
               type="text"
               placeholder="Gift card or Discount code"
+              style={{width:"80%"}}
             ></Long>
             <button
               style={{
                 color: "gray",
                 backgroundColor: "lightgray",
-                padding: 10,
-                width: 70,
+                width: 100,
+                height:50,
                 borderRadius: 7,
                 border: "none",
               }}
-              type="submit"
-              placeholder="Apply"
             >
               Apply
             </button>
           </div>
           <hr />
-          <div>
+          <div style={{ display: "flex",justifyContent:"space-between"}}>
             <h5>Subtotal</h5>
+            <h5>320</h5>
+          </div>
+          <div style={{ display: "flex",justifyContent:"space-between"}}>
             <h5>Shipping</h5>
+            <h5>free</h5>
           </div>
           <hr />
-          <h4>Toatal</h4>
-        </form>
+          <div style={{ display: "flex",justifyContent:"space-between"}}>
+            <h3>Total</h3>
+            <h3>INR ₹ 320.00</h3>
+          </div>
       </Total>
     </Check>
   );
