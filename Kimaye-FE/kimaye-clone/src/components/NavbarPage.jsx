20ea17cb6fe5fb7bf5b737aae5d6ae64bd77718d
Akshit Rana <akshitrana21@gmail.com>
2022-06-18 12:07:34 +0530

    Minor css changes

--- Kimaye-FE/kimaye-clone/src/components/NavbarPage.jsx
+++ Kimaye-FE/kimaye-clone/src/components/NavbarPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./styles/navbar.css";
+import "./styles/navbar.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { Nav, Navbar, NavDropdown, NavLink } from "react-bootstrap";
 
@@ -11,13 +11,26 @@ const dropdowns = {
 
 const NavbarPage = () => {
   return (
-    <Navbar bg="myColor" variant="#EEEEEA" sticky="top" style={{ ...dropdowns }}>
+    <Navbar
+      bg="myColor"
+      variant="#EEEEEA"
+      sticky="top"
+      style={{ ...dropdowns }}
+    >
       <Nav style={{ gap: "30px", color: "black" }}>
         <NavDropdown title="SHOP">
-          <NavDropdown.Item href="/collections/allfruits">ALL FRUITS</NavDropdown.Item>
-          <NavDropdown.Item href="/collections/cut-fruits">FRESH CUTS</NavDropdown.Item>
-          <NavDropdown.Item href="/collections/combo-fruits">FRUIT COMBOS</NavDropdown.Item>
-          <NavDropdown.Item href="/collections/gifts">GIFTS BY KIMAYE</NavDropdown.Item>
+          <NavDropdown.Item href="/collections/allfruits">
+            ALL FRUITS
+          </NavDropdown.Item>
+          <NavDropdown.Item href="/collections/cut-fruits">
+            FRESH CUTS
+          </NavDropdown.Item>
+          <NavDropdown.Item href="/collections/combo-fruits">
+            FRUIT COMBOS
+          </NavDropdown.Item>
+          <NavDropdown.Item href="/collections/gifts">
+            GIFTS BY KIMAYE
+          </NavDropdown.Item>
           <NavDropdown.Item href="/"> GRAPE FAMILY BY KIMAYE</NavDropdown.Item>
           <NavDropdown.Item href="/">RETURN AND REPLACEMENT</NavDropdown.Item>
           <NavDropdown.Item href="/">COMMUNITY BUYING</NavDropdown.Item>
@@ -34,15 +47,30 @@ const NavbarPage = () => {
       </Nav>
       <Nav>
         <NavLink href="/">
-          <img className="logo"href="/" src="https://cdn.shopify.com/s/files/1/0449/5225/6667/files/website-logo_400x.png?v=1596288204"/>
+          <img
+            className="logo"
+            href="/"
+            src="https://cdn.shopify.com/s/files/1/0449/5225/6667/files/website-logo_400x.png?v=1596288204"
+          />
         </NavLink>
       </Nav>
 
       <Nav style={{ display: "flex", gap: "30px" }}>
-        <NavLink> <i class="fa-solid fa-location-dot"></i></NavLink>
-        <NavLink> <i class="fa-solid fa-magnifying-glass"></i></NavLink>
-        <NavLink href="/login"> <i class="fa-solid fa-user"></i></NavLink>
-        <NavLink href="/cart"><i class="fa-solid fa-bag-shopping"></i></NavLink>
+        <NavLink>
+          {" "}
+          <i class="fa-solid fa-location-dot"></i>
+        </NavLink>
+        <NavLink>
+          {" "}
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </NavLink>
+        <NavLink href="/login">
+          {" "}
+          <i class="fa-solid fa-user"></i>
+        </NavLink>
+        <NavLink href="/cart">
+          <i class="fa-solid fa-bag-shopping"></i>
+        </NavLink>
       </Nav>
     </Navbar>
   );
